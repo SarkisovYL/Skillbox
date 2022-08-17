208ef5878a7b69a68e73dbf084441f2257df2cb1
Sarkisov Yuri <sarkisov.yl@yandex.ru>
2022-08-17 17:15:51 +0300

    Second commit

--- src/main/kotlin/module_10/lesson1/main.kt
+++ src/main/kotlin/module_10/lesson1/main.kt
@@ -5,6 +5,8 @@ fun main() {
     devices.forEach{it.call("+7(999) 999-99-99") }
 
     val mobilePhone = MobilePhone()
+    val radioPhone = RadioPhone()
+    radioPhone.receiveCall("158-158")
     mobilePhone.call(number = "+7(999) 999-99-99")
     mobilePhone.playVideo(url = "www.example.com")
 
