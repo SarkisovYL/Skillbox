2cec810b1b02c6f50d187a1c046ea98d61950cba
Sarkisov Yuri <sarkisov.yl@yandex.ru>
2022-08-31 10:49:28 +0300

    small correct commit

--- src/main/kotlin/module_12/Generics_And_Utility_Classes_Throwable/NoAmmoException.kt
+++ src/main/kotlin/module_12/Generics_And_Utility_Classes_Throwable/NoAmmoException.kt
@@ -1,7 +1,5 @@
 package module_12.Generics_And_Utility_Classes_Throwable
 
 class NoAmmoException (message: String) : Exception(message){
-    init {
-        println(message)
-    }
+    init { println(message) }
 }
